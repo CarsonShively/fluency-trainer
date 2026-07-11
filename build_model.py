7b42f082aad826f58e1616ff7e4faf90b9ba141c
Carson Shively <carsonshively24@gmail.com>
2026-07-11 13:47:03 -0700

    backup

--- build_model.py
+++ build_model.py
@@ -1,68 +1,96 @@
-from fluency_trainer.samples_to_matrices import samples_to_matrices
-from fluency_trainer.target_phones_matrix import target_phones_matrix
-from fluency_trainer.user_audio_matrix import user_audio_matrix
-from fluency_trainer.pad_target import pad_target
-from fluency_trainer.matrices_to_tensors import matrices_to_tensors
 from fluency_trainer.user_phoneme_encoder import UserPhonemeEncoder
 from fluency_trainer.target_phoneme_encoder import TargetPhonemeEncoder
 from fluency_trainer.cross_attention_transformer import CrossAttentionTransformer
 from fluency_trainer.classifier_head import ClassifierHead
 from fluency_trainer.train import train
 
+import numpy as np
 import tensorflow as tf
 from pathlib import Path
 import shutil
 import json
+from huggingface_hub import snapshot_download
+
+# snapshot == function that executes and returns nothing
+# masks float vs int
+# data.from tensor slices == takes in touple 
+# load train and val free then load test
+# go lower in the backprop?
+# only shuffle train batches
+# cache here tracks not stores (doesnt duplicate)
+# google drive cache?
+# hf local vs cache?
+# snapshot no local dir == returns a string cache path (put inside Path() to return path object) 
 
 def build_model():
-    print("starting...")
-    samples_path = Path(__file__).resolve().parents[0] / "data/samples"
     
-    with open(samples_path / "train.json", "r") as con:
-        train_samples = json.load(con)
+    cache_data = Path(snapshot_download(
+        repo_id="Carson-Shively/fluency-trainer",
+        repo_type="dataset",
+        allow_patterns="matrices/**"
+    ))
     
-    with open(samples_path / "val.json", "r") as con:
-        val_samples = json.load(con)
-    
-    with open(samples_path / "test.json", "r") as con:
-        test_samples = json.load(con)
-    
-    print("train, val, and test samples loaded")
-    
-    vocab_path = Path(__file__).resolve().parents[0] / "data/vocab/vocab.json"
-    with open(vocab_path, "r") as con:
-        vocab = json.load(con)
+    train_user_audio = np.load(cache_data / "matrices/train_user_audio.npy")
+    val_user_audio = np.load(cache_data / "matrices/val_user_audio.npy")
+    test_user_audio = np.load(cache_data / "matrices/test_user_audio.npy")
 
-    print(f"vocab length: {len(vocab)}")
+    train_user_audio_mask = np.load(cache_data / "matrices/train_user_audio_mask.npy")
+    val_user_audio_mask = np.load(cache_data / "matrices/val_user_audio_mask.npy")
+    test_user_audio_mask = np.load(cache_data / "matrices/test_user_audio_mask.npy")
+
+    train_target_phonemes = np.load(cache_data / "matrices/train_target_phonemes.npy")
+    val_target_phonemes = np.load(cache_data / "matrices/val_target_phonemes.npy")
+    test_target_phonemes = np.load(cache_data / "matrices/test_target_phonemes.npy")
+
+    train_target_phonemes_mask = np.load(cache_data / "matrices/train_target_phonemes_mask.npy")
+    val_target_phonemes_mask = np.load(cache_data / "matrices/val_target_phonemes_mask.npy")
+    test_target_phonemes_mask = np.load(cache_data / "matrices/test_target_phonemes_mask.npy")
     
-    train_user_audio, train_target_phones, train_target_classes, val_user_audio, val_target_phones, val_target_classes, test_user_audio, test_target_phones, test_target_classes = samples_to_matrices(train_samples=train_samples, val_samples=val_samples, test_samples=test_samples)
     
-    train_target_phones_matrix = target_phones_matrix(target_phones=train_target_phones, vocab=vocab)
-    val_target_phones_matrix = target_phones_matrix(target_phones=val_target_phones, vocab=vocab)
-    test_target_phones_matrix = target_phones_matrix(target_phones=test_target_phones, vocab=vocab)
+    train_target_classes = np.load(cache_data / "matrices/train_target_classes.npy")
+    val_target_classes = np.load(cache_data / "matrices/val_target_classes.npy")
+    test_target_classes = np.load(cache_data / "matrices/test_target_classes.npy")
+
+    train_target_classes_mask = np.load(cache_data / "matrices/train_target_classes_mask.npy")
+    val_target_classes_mask = np.load(cache_data / "matrices/val_target_classes_mask.npy")
+    test_target_classes_mask = np.load(cache_data / "matrices/test_target_classes_mask.npy")
     
-    padded_train_user_audio_matrix, train_user_audio_matrix_mask, audio_max_len, uncertianty_vector_size = user_audio_matrix(user_audio_paths=train_user_audio)
-    padded_val_user_audio_matrix, val_user_audio_matrix_mask, audio_max_len, uncertianty_vector_size = user_audio_matrix(user_audio_paths=val_user_audio)
-    padded_test_user_audio_matrix, test_user_audio_matrix_mask, audio_max_len, uncertianty_vector_size = user_audio_matrix(user_audio_paths=test_user_audio)
+    print("point 1")
     
-    padded_train_target_phones_matrix, padded_train_target_classes_matrix, max_target_len = pad_target(target_phones=train_target_phones_matrix, target_classes=train_target_classes)
-    padded_val_target_phones_matrix, padded_val_target_classes_matrix, max_target_len = pad_target(target_phones=val_target_phones_matrix, target_classes=val_target_classes)
-    padded_test_target_phones_matrix, padded_test_target_classes_matrix, max_target_len = pad_target(target_phones=test_target_phones_matrix, target_classes=test_target_classes)
+    train_user_audio_tensor = tf.convert_to_tensor(train_user_audio, dtype=tf.float32)
+    val_user_audio_tensor = tf.convert_to_tensor(val_user_audio, dtype=tf.float32)
+    test_user_audio_tensor = tf.convert_to_tensor(test_user_audio, dtype=tf.float32)
+
+    train_user_audio_mask_tensor = tf.convert_to_tensor(train_user_audio_mask, dtype=tf.float32)
+    val_user_audio_mask_tensor = tf.convert_to_tensor(val_user_audio_mask, dtype=tf.float32)
+    test_user_audio_mask_tensor = tf.convert_to_tensor(test_user_audio_mask, dtype=tf.float32)
+
+    train_target_phonemes_tensor = tf.convert_to_tensor(train_target_phonemes, dtype=tf.int32)
+    val_target_phonemes_tensor = tf.convert_to_tensor(val_target_phonemes, dtype=tf.int32)
+    test_target_phonemes_tensor = tf.convert_to_tensor(test_target_phonemes, dtype=tf.int32)
+
+    train_target_phonemes_mask_tensor = tf.convert_to_tensor(train_target_phonemes_mask, dtype=tf.int32)
+    val_target_phonemes_mask_tensor = tf.convert_to_tensor(val_target_phonemes_mask, dtype=tf.int32)
+    test_target_phonemes_mask_tensor = tf.convert_to_tensor(test_target_phonemes_mask, dtype=tf.int32)
     
-    train_user_audio_tensor, train_target_phones_tensor, train_target_classes_tensor, train_target_phones_mask_tensor, train_target_classes_mask_tensor = matrices_to_tensors(user_audio=padded_train_user_audio_matrix, target_phones=padded_train_target_phones_matrix, target_classes=padded_train_target_classes_matrix)
-    val_user_audio_tensor, val_target_phones_tensor, val_target_classes_tensor, val_target_phones_mask_tensor, val_target_classes_mask_tensor = matrices_to_tensors(user_audio=padded_val_user_audio_matrix, target_phones=padded_val_target_phones_matrix, target_classes=padded_val_target_classes_matrix)
-    test_user_audio_tensor, test_target_phones_tensor, test_target_classes_tensor, test_target_phones_mask_tensor, test_target_classes_mask_tensor = matrices_to_tensors(user_audio=padded_test_user_audio_matrix, target_phones=padded_test_target_phones_matrix, target_classes=padded_test_target_classes_matrix)
-    train_user_audio_mask_tensor = tf.convert_to_tensor(train_user_audio_matrix_mask.cpu().numpy(), dtype=tf.float32)
-    val_user_audio_mask_tensor = tf.convert_to_tensor(val_user_audio_matrix_mask.cpu().numpy(), dtype=tf.float32)
-    test_user_audio_mask_tensor = tf.convert_to_tensor(test_user_audio_matrix_mask.cpu().numpy(), dtype=tf.float32)
+    
+    train_target_classes_tensor = tf.convert_to_tensor(train_target_classes, dtype=tf.int32)
+    val_target_classes_tensor = tf.convert_to_tensor(val_target_classes, dtype=tf.int32)
+    test_target_classes_tensor = tf.convert_to_tensor(test_target_classes, dtype=tf.int32)
+
+    train_target_classes_mask_tensor = tf.convert_to_tensor(train_target_classes_mask, dtype=tf.int32)
+    val_target_classes_mask_tensor = tf.convert_to_tensor(val_target_classes_mask, dtype=tf.int32)
+    test_target_classes_mask_tensor = tf.convert_to_tensor(test_target_classes_mask, dtype=tf.int32)
+    
+    print("point 2")
     
     
     train_dataset = tf.data.Dataset.from_tensor_slices((
         {
             "user_audio": train_user_audio_tensor,
             "user_audio_mask": train_user_audio_mask_tensor,
-            "target_phones": train_target_phones_tensor,
-            "target_phones_mask": train_target_phones_mask_tensor
+            "target_phones": train_target_phonemes_tensor,
+            "target_phones_mask": train_target_phonemes_mask_tensor
         },
         train_target_classes_tensor,
         train_target_classes_mask_tensor
@@ -73,8 +101,8 @@ def build_model():
         {
             "user_audio": val_user_audio_tensor,
             "user_audio_mask": val_user_audio_mask_tensor,
-            "target_phones": val_target_phones_tensor,
-            "target_phones_mask": val_target_phones_mask_tensor
+            "target_phones": val_target_phonemes_tensor,
+            "target_phones_mask": val_target_phonemes_mask_tensor
         },
         val_target_classes_tensor,
         val_target_classes_mask_tensor
@@ -84,17 +112,22 @@ def build_model():
         {
             "user_audio": test_user_audio_tensor,
             "user_audio_mask": test_user_audio_mask_tensor,
-            "target_phones": test_target_phones_tensor,
-            "target_phones_mask": test_target_phones_mask_tensor
+            "target_phones": test_target_phonemes_tensor,
+            "target_phones_mask": test_target_phonemes_mask_tensor
         },
         test_target_classes_tensor,
         test_target_classes_mask_tensor
     ))
     
-    train_dataset_batched = train_dataset.shuffle(len(train_target_classes_tensor)).batch(16)
-    val_dataset_batched = val_dataset.shuffle(len(val_target_classes_tensor)).batch(16)
-    test_dataset_batched = test_dataset.shuffle(len(test_target_classes_tensor)).batch(16)
+    print("point 3")
     
+    train_dataset_batched = train_dataset.shuffle(len(train_target_classes_tensor)).batch(16)
+    val_dataset_batched = val_dataset.batch(16)
+    test_dataset_batched = test_dataset.batch(16)
+    
+    print("load successful")
+    
+    """ 
     user_audio_encoder = UserPhonemeEncoder(max_phonemes=audio_max_len, uncertianty_vector_size=uncertianty_vector_size, dense=64, dense1=128)
     target_phoneme_encoder = TargetPhonemeEncoder(vocab_size=vocab_size, max_phonemes=max_target_len, embeded_vector_size=128, dense=64, dense1=128)
     cross_attention_transformer = CrossAttentionTransformer(embeded_vector_size=128, desne=64, dense1=128)
@@ -105,7 +138,8 @@ def build_model():
     loss_fn = tf.losses.SparseCategoricalCrossentropy(from_logits=True, reduction="none")
     
     val_loss = train(train_dataset=train_dataset_batched, val_dataset=val_dataset_batched, user_encoder=user_audio_encoder, target_encoder=target_phoneme_encoder, transformer=cross_attention_transformer, classifier=classifier_head, optimizer=optimizer, loss_fn=loss_fn)
-    
+
+   
     total_holdout_loss = 0
     batch_counter = 0
     for batch, classes, mask in test_dataset_batched:
@@ -140,6 +174,7 @@ def build_model():
     
     with open(out_path / "metrics.json", "w") as con:
         json.dump(metrics, con)
+    """
         
 if __name__ == "__main__":
     build_model()
